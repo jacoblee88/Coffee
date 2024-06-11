47d44b41e706b3b3d61c281e17b10274bfe24f29
Jacob <jacobleeyo@gmail.com>
2024-06-11 11:26:27 -0400

    Initial Commit

---
+++ XiaoJia_Coffee/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XiaoJia_Coffee
+//
+//  Created by Jacob Lee on 2024-06-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XiaoJia_Coffee/XiaoJia_CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XiaoJia_CoffeeApp.swift
+//  XiaoJia_Coffee
+//
+//  Created by Jacob Lee on 2024-06-11.
+//
+
+import SwiftUI
+
+@main
+struct XiaoJia_CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
